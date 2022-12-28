8f50a67f3403f7c5e3e7a4feb31f42a788345971
Felix Dijkstal <48564864+fsdijkstal@users.noreply.github.com>
2022-12-28 15:29:23 +0000

    Add files via upload

--- server.R
+++ server.R
@@ -1,15 +1,6 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-data = read.csv('/Users/felixdijkstal/Downloads/archive/wcmatches.csv')
+data = read.csv('~/worldcup/wcmatches.csv')
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
